d4929c79ac5cf699567bd702842e1d0cf24ad672
Pablo Macuada <pablo.macuada@usach.cl>
2024-04-08 23:17:15 -0400

    Pablo Macuada
    Creación de la entidad Emergencia

--- Backend/src/main/java/Backend_Voluntarios/Backend/Entity/Entity_habilidad.java
+++ Backend/src/main/java/Backend_Voluntarios/Backend/Entity/Entity_habilidad.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Habilidad")
-public class Entity_habilidad {
+public class Entity_Habilidad {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
@@ -12,7 +12,7 @@ public class Entity_habilidad {
 
     private String nombre_habilidad;
 
-    public Entity_habilidad(Long id_habilidad, String nombre_habilidad){
+    public Entity_Habilidad(Long id_habilidad, String nombre_habilidad) {
         this.Id_habilidad = id_habilidad;
         this.nombre_habilidad = nombre_habilidad;
     }
