b789b5bf0a3d662fd02231f267197ffd29afed41
haruhisomu <nicolas.salinas.r@usach.cl>
2024-04-11 22:10:37 -0400

    Actualización de Estado Tarea y de Habilidad

--- Backend/src/main/java/Backend_Voluntarios/Backend/Entity/Entity_Habilidad.java
+++ Backend/src/main/java/Backend_Voluntarios/Backend/Entity/Entity_Habilidad.java
@@ -2,8 +2,9 @@ package Backend_Voluntarios.Backend.Entity;
 
 import jakarta.persistence.*;
 
+
 @Entity
-@Table(name = "Habilidad")
+@Table(name="Habilidad")
 public class Entity_Habilidad {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +15,21 @@ public class Entity_Habilidad {
 
     public Entity_Habilidad(Long id_habilidad, String nombre_habilidad) {
         this.Id_habilidad = id_habilidad;
+        this.nombre_habilidad = nombre_habilidad;
+    }
+    // constructor vacio
+    public Entity_Habilidad(){
+        super();
+    }
+    // geters
+    public Long getId_habilidad() {
+        return Id_habilidad;
+    }
+    public String getNombre_habilidad() {
+        return nombre_habilidad;
+    }
+    // seter
+    public void setNombre_habilidad(String nombre_habilidad) {
         this.nombre_habilidad = nombre_habilidad;
     }
 }
