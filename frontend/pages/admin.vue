8456af3de8829f9a356562a2a548eb403e7c76f4
Pablo Macuada <pablo.macuada@usach.cl>
2024-04-19 17:18:45 -0400

    Creación de vista admin

---
+++ frontend/pages/admin.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <img src="../images/mundo.svg" alt="mundo">
+    <Navbar />
+    <main></main>
+
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+body {
+    margin: 0;
+    color: black;
+    background-color: black;
+    font-family: 'Roboto', sans-serif;
+}
+
+
+
+img {
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    z-index: -5;
+    object-fit: cover;
+    filter: brightness(0.25) invert(1);
+}
+</style>
