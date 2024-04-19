4146f3931458774fe9d01ba008385987f73549a1
Macarena <macarena.soto.h@usach.cl>
2024-04-19 17:20:08 -0400

    Cambios de prueba

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,26 @@
-<template>
+<!-- <template>
   <div>
+    <div>
+      <div class="more" @click="$router.push(/login);" >Ver más</div>
+    </div>
     <ul>
       <li>
         <NuxtLink to="/componentep">Holaaa</NuxtLink>
       </li>
     </ul>
   </div>
+</template> -->
+
+<template>
+
+  <div>
+    <p>Holaaaaaaa</p>
+    <p>aaaaaaaaaaaa</p>
+    <div class="more"  @click="$router.push(`/login`);" >Ver más</div>
+  </div>
+
 </template>
+
+<script>
+
+</script>
