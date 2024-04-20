778f13ad4dc216e9799ec880937fcbf86efcd3f4
Macarena <macarena.soto.h@usach.cl>
2024-04-20 16:16:54 -0400

    Modificaciones Navbar

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,44 +1,92 @@
 <template>
-    <nav>
-        <div class="menu">
-            <!-- menu desplegable -->
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Servicios</a></li>
-                <li><a href="#">Contacto</a></li>
-            </ul>
-        </div>
-    </nav>
+  <nav>
+    <div class="logo">
+      <h1>Nombre fabuloso</h1>
+    </div>
+    <div class="menu">
+      <!-- Menú principal -->
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Servicios</a></li>
+        <!-- Elemento con menú desplegable -->
+        <li class="dropdown" @mouseover="showDropdown" @mouseleave="hideDropdown">
+          <a href="#">Estadísticas <span>&#9662;</span></a>
+          <!-- Submenú desplegable -->
+          <ul class="dropdown-content" v-show="isDropdownVisible">
+            <li><a href="#">Ranking Voluntario por Tarea</a></li>
+            <li><a href="#">Ranking Voluntario por Emergencia</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      isDropdownVisible: false
+    };
+  },
+  methods: {
+    showDropdown() {
+      this.isDropdownVisible = true;
+    },
+    hideDropdown() {
+      this.isDropdownVisible = false;
+    }
+  }
+};
 </script>
 
 <style>
 nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 5%;
-    background-color: rgba(255, 255, 255, 0.7);
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 5%;
+  background-color: rgba(255, 255, 255, 0.7);
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 
-.logo img {
-    width: 100px;
+.logo h1 {
+  margin: 0;
+  font-size: 20px;
+  font-weight: bold;
+  color: #333;
 }
 
 .menu ul {
-    display: flex;
-    list-style: none;
+  display: flex;
+  list-style: none;
 }
 
 .menu ul li {
-    margin-right: 20px;
+  margin-right: 20px;
 }
 
 .menu ul li a {
-    text-decoration: none;
-    color: #333;
+  text-decoration: none;
+  color: #333;
+}
+
+/* Estilos para el menú desplegable */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content li {
+  padding: 10px;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
 }
 </style>
