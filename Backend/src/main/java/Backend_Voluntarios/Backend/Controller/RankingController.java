fb8a00d6d6246bc6f08b048532eac2ae8fd7d5fe
Nicolás Muñoz <nicolas.munoz.r@usach.cl>
2024-04-20 15:33:53 -0400

    Modificación de los controllers de Voluntarios y ranking

--- Backend/src/main/java/Backend_Voluntarios/Backend/Controller/RankingController.java
+++ Backend/src/main/java/Backend_Voluntarios/Backend/Controller/RankingController.java
@@ -1,18 +1,29 @@
 package Backend_Voluntarios.Backend.Controller;
 
 import Backend_Voluntarios.Backend.Entity.RankingEntity;
+import Backend_Voluntarios.Backend.Entity.TareaEntity;
+import Backend_Voluntarios.Backend.Entity.TareaHabilidadEntity;
+import Backend_Voluntarios.Backend.Entity.VoluntarioEntity;
 import Backend_Voluntarios.Backend.Service.RankingService;
+import Backend_Voluntarios.Backend.Service.TareaHabilidadService;
+import Backend_Voluntarios.Backend.Service.TareaService;
+import Backend_Voluntarios.Backend.Service.VoluntarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ranking")
 public class RankingController {
     @Autowired
-    private RankingService serviceRanking;
+    private RankingService rankingService;
+    private VoluntarioService voluntarioService;
+    private TareaService tareaService;
+    private TareaHabilidadService tareaHabilidadService;
 
     @GetMapping()
     public String conectado(){
@@ -21,16 +32,21 @@ public class RankingController {
 
     @GetMapping("/all")
     public List<RankingEntity> tabla(){
-        return serviceRanking.tablaCompleta();
+        return rankingService.tablaCompleta();
     }
 
     @GetMapping("/{palabraClave}")
-    public ResponseEntity<List<RankingEntity>> Buscar_rankings(@PathVariable String palabraClave){
-        List<RankingEntity> rankingsEncontrados =  serviceRanking.listaFiltro(palabraClave);
+    public ResponseEntity<List<RankingEntity>> buscarRankings(@PathVariable String palabraClave){
+        List<RankingEntity> rankingsEncontrados =  rankingService.listaFiltro(palabraClave);
         if (rankingsEncontrados.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(rankingsEncontrados);
+    }
+
+    @GetMapping("/listaRanking")
+    public List<RankingEntity> listaRanking(){
+        return rankingService.listaRanking();
     }
 
     @GetMapping("/{idRanking}")
@@ -38,25 +54,43 @@ public class RankingController {
         if (idRanking == null) {
             return ResponseEntity.badRequest().build();
         }
-        List<RankingEntity> idRankingsEncontrados = serviceRanking.tablaId(idRanking);
+        List<RankingEntity> idRankingsEncontrados = rankingService.tablaId(idRanking);
         if (idRankingsEncontrados.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(idRankingsEncontrados);
     }
 
-    @PostMapping("/guardar")
-    public ResponseEntity<RankingEntity> crearVoluntario(@PathVariable RankingEntity rankingEntity) {
-        if (rankingEntity.getIdRanking() != null) {
-            return ResponseEntity.badRequest().build();
+    @PostMapping("/guardar/{idVoluntario}/{idEmergencia}")
+    public void crearRanking(@PathVariable Long idVoluntario,
+                                      @PathVariable Long idEmergencia) {
+        List<TareaEntity> tareas = tareaService.tablaIds(idEmergencia);
+        for (TareaEntity tarea : tareas) {
+            Long idTarea = tarea.getIdTarea();
+            TareaEntity nombreTarea = tareaService.getTareaById(idTarea);
+            String tareaRanking = nombreTarea.getNombreTarea();
+            VoluntarioEntity buscarZona = voluntarioService.buscarId(idVoluntario);
+            String zona = buscarZona.getZonaViviendaVoluntario();
+            List<String> equipamiento = buscarZona.getEquipamientoVoluntario();
+            int nivelRanking = rankingService.puntajeRanking(zona, equipamiento);
+            RankingEntity ranking = new RankingEntity(idTarea, idVoluntario, nivelRanking, tareaRanking);
+            rankingService.nuevoRanking(ranking);
         }
-        serviceRanking.nuevoRanking(rankingEntity);
-        return ResponseEntity.ok(rankingEntity);
     }
 
     @DeleteMapping("/delete/{idRanking}")
     public void eliminar(@PathVariable Long idRanking){
-        RankingEntity rankingEntity = serviceRanking.buscarId(idRanking);
-        serviceRanking.borrarRanking(rankingEntity);
+        RankingEntity rankingEntity = rankingService.buscarId(idRanking);
+        rankingService.borrarRanking(rankingEntity);
+    }
+
+    @PutMapping("editar}/{idVoluntario}")
+    public void actualizar(@PathVariable Long idVoluntario){
+        VoluntarioEntity update = voluntarioService.buscarId(idVoluntario);
+        List<String> equipo = update.getEquipamientoVoluntario();
+        String zona = update.getZonaViviendaVoluntario();
+        RankingEntity updateUser = rankingService.buscarId(idVoluntario);
+        updateUser.setNivelRanking(rankingService.puntajeRanking(zona, equipo));
+        rankingService.nuevoRanking(updateUser);
     }
 }
