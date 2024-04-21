6a31725e5a3c50766d3a72b2e1b79c994d575ba1
Macarena <macarena.soto.h@usach.cl>
2024-04-20 20:28:05 -0400

    Arreglos vista Voluntario

--- frontend/pages/voluntario.vue
+++ frontend/pages/voluntario.vue
@@ -2,7 +2,12 @@
   <div class="main-conteiner">
       <div class="background-container"> 
         <img src="../images/mundo.svg" alt="mundo">
-        <Navbar />
+        <NavbarVoluntario  @change-component="changeComponent" />
+        <!-- Mostrar ListCard si mostrarListCard es verdadero -->
+    <!-- <ListCard v-if="mostrarListCard" /> -->
+
+<!-- Mostrar ListCard2 si mostrarListCard es falso -->
+
         <div id="app">
           <side-menu></side-menu>
           <router-view></router-view>
@@ -14,10 +19,35 @@
           >
             Contenido de la tarjeta.
           </ListCard> -->
-        <ListCard title1="Tareas de Emergencias inscritas" description1="Lorem Ipsum es mente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galeríagresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de psum es mente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galeríagresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lopsum es mente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galeríagresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lopsum es mente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galeríagresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lopsum es mente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galeríagresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lopsum es mente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galeríagresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lopsum es mente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galeríagresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de LoLorem Ipsum." title2="TAREAS" description2="aloooooooooooodnsnijnijb">
-            Contenido de la primera tarjeta.
+        <!-- Mostrar ComponenteA si mostrarComponenteA es verdadero -->
+        <!-- <ListCard v-if="mostrarComponenteA" /> -->
+
+        <!-- Mostrar ComponenteB si mostrarComponenteA es falso -->
+        <!-- <ListCard2 v-else /> -->
+        <ListCard v-if="mostrarListCard"
+        title1 = "Tareas de Emergencias inscritas"
+        :description1="[
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'En proceso'},
+            {text1:'Recoger escombros',text2:'Incendio en Valparaíso', buttonLabel:'En proceso'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Terminada'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Terminada'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Terminada'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'En pausa'}]"
+        title2= "TAREAS"
+        :description2="[
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Postular'},
+            {text1:'Recoger escombros',text2:'Incendio en Valparaíso', buttonLabel:'Aceptar'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Aceptar'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Aceptar'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Aceptar'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Aceptar'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Aceptar'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Aceptar'},
+            {text1:'Primeros Auxilios',text2:'Incencio en Valparaíso' ,buttonLabel:'Aceptar'}]">
         </ListCard>
+        <ListCard2 v-else />
         <main></main>
+
         
     </div>
   </div>
@@ -29,17 +59,32 @@
 
 import SideMenu from '@/components/SideMenu.vue';
 import ListCard from '@/components/ListCard.vue';
+import ListCard2 from '@/components/ListCard2.vue';
+import NavbarVoluntario from '@/components/NavbarVoluntario.vue';
 import Footer  from '@/components/Footer.vue';
 
 export default {
+
   components: {
-    ListCard
+    ListCard,
+    ListCard2
   },
   data() {
     return {
-      backendData: null
+      backendData: null,
+      mostrarComponenteA: true ,// Mostrar ListCard por defecto// Mostrar ComponenteA por defecto
+      mostrarListCard: true 
     };
   },
+  methods:{
+    toggleComponent() {
+      // Cambia el estado para mostrar el componente contrario
+      this.mostrarComponenteA = !this.mostrarComponenteA;
+    },
+    changeComponent(value) {
+      this.mostrarListCard = value;
+    }
+  }
   /* mounted() {
     // Realiza la solicitud HTTP al backend para obtener los datos
     axios.get("URL_DEL_BACKEND")
